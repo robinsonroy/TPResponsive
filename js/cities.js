1887cbae36f7028be521496a2149bd81330ffd7f
Lionel Grondin <lionelgrondin2@gmail.com>
2016-11-29 10:53:29 +0100

    Ng-repeat

--- js/cities.js
+++ js/cities.js
@@ -1,49 +1,73 @@
 var cities = [
 {
     name: 'Paris',
-    dates: '01 - 04 décembre',
+    datemin: '01',
+    daymin: '2016-12-01',
+    datemax : '04 décembre',
+    daymax : '2016-12-04',
     description: '3 nuits - 55€ la nuit',
     price: 165
 },
 {
     name: 'Londres',
-    dates: '13 - 18 décembre',
+    datemin: '13',
+    daymin: '2016-12-13',
+    datemax : '18 décembre',
+    daymax : '2016-12-18',
     description: '5 nuits - 40€ la nuit',
     price: 200
 },
 {
     name: 'Madrid',
-    dates: '02 - 10 décembre',
+    datemin: '02',
+    daymin: '2016-12-02',
+    datemax : '10 décembre',
+    daymax : '2016-12-10',
     description: '8 nuits - 47€ la nuit',
     price: 376
 },
 {
     name: 'Rome',
-    dates: '08 - 10 décembre',
+    datemin: '08',
+    daymin: '2016-12-08',
+    datemax : '10 décembre',
+    daymax : '2016-12-10',
     description: '2 nuits - 52€ la nuit',
     price: 104
 },
 {
     name: 'Lisbonne',
-    dates: '01 - 09 décembre',
+    datemin: '01',
+    daymin: '2016-12-01',
+    datemax : '09 décembre',
+    daymax : '2016-12-09',
     description: '8 nuits - 34€ la nuit',
     price: 287
 },
 {
     name: 'Berlin',
-    dates: '03 - 10 décembre',
+    datemin: '03',
+    daymin: '2016-12-03',
+    datemax : '10 décembre',
+    daymax : '2016-12-10',
     description: '7 nuits - 41€ la nuit',
     price: 287
 },
 {
     name: 'Athènes',
-    dates: '05 - 09 décembre',
+    datemin: '05',
+    daymin: '2016-12-05',
+    datemax : '09 décembre',
+    daymax : '2016-12-09',
     description: '4 nuits - 40€ la nuit',
     price: 160
 },
 {
     name: 'Prague',
-    dates: '12 - 17 décembre',
+    datemin: '12',
+    daymin: '2016-12-12',
+    datemax : '17 décembre',
+    daymax : '2016-12-17',
     description: '5 nuits - 60€ la nuit',
     price: 300
 },
