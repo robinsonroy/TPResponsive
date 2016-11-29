cf2b8adb30bc93d92b1e69da7c905d9392f5aef7
robinson Roy <robinson.roy@me.com>
2016-11-29 09:18:58 +0100

    first commit

---
+++ js/cities.js
@@ -0,0 +1,50 @@
+var cities = [
+{
+    name: 'Paris',
+    dates: '01 - 04 décembre',
+    description: '3 nuits - 55€ la nuit',
+    price: 165
+},
+{
+    name: 'Londres',
+    dates: '13 - 18 décembre',
+    description: '5 nuits - 40€ la nuit',
+    price: 200
+},
+{
+    name: 'Madrid',
+    dates: '02 - 10 décembre',
+    description: '8 nuits - 47€ la nuit',
+    price: 376
+},
+{
+    name: 'Rome',
+    dates: '08 - 10 décembre',
+    description: '2 nuits - 52€ la nuit',
+    price: 104
+},
+{
+    name: 'Lisbonne',
+    dates: '01 - 09 décembre',
+    description: '8 nuits - 34€ la nuit',
+    price: 287
+},
+{
+    name: 'Berlin',
+    dates: '03 - 10 décembre',
+    description: '7 nuits - 41€ la nuit',
+    price: 287
+},
+{
+    name: 'Athènes',
+    dates: '05 - 09 décembre',
+    description: '4 nuits - 40€ la nuit',
+    price: 160
+},
+{
+    name: 'Prague',
+    dates: '12 - 17 décembre',
+    description: '5 nuits - 60€ la nuit',
+    price: 300
+},
+];
